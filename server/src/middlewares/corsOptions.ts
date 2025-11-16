5f19d2694b0646cdff452309b8218568eaca3149
microbjj <1705888@mail.ru>
2025-11-16 19:22:47 +0700

    update cors

--- server/src/middlewares/corsOptions.ts
+++ server/src/middlewares/corsOptions.ts
@@ -1,7 +1,12 @@
 import { CorsOptions } from "cors"
 
 const devOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"]
-const prodOrigins = ["https://example.com"]
+const prodOrigins = [
+    "https://getonlyneed.ru",
+    "https://www.getonlyneed.ru",
+    "http://getonlyneed.ru",
+    "http://www.getonlyneed.ru",
+]
 
 export const getCorsOptions = (): CorsOptions => {
     const isProd = process.env.NODE_ENV === "production"
